a01ee01a4063653a276a95b0a83f7568f80ba290
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-17 13:37:41 +0700

    feat: implement erd with sql syntax

--- challenge.sql
+++ challenge.sql
@@ -9,17 +9,43 @@ CREATE TABLE user_roles (
   id_user int NOT NULL
 );
 CREATE TABLE roles (
-  id SERIAL PRIMARY
-  name VARCHAR(80) UNIQUE
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(80) NOT NULL
 );
 
-alter table user_roles (
+alter table user_roles 
 add constraint fk_ur_users FOREIGN KEY (id_user)
-REFERENCES users(id));
+REFERENCES users(id);
 
-ALTER TABLE user_roles (
+ALTER TABLE user_roles 
 add constraint fk_ur_roles FOREIGN KEY (id_role)
-REFERENCES roles(id));
+REFERENCES roles(id);
+
+ALTER TABLE roles
+ADD constraint roles_unique UNIQUE(name);
+
+insert into users(name)
+VALUES
+('Budi'),
+('Toni'),
+('Putri'),
+('Dian'),
+('Gunawan');
+
+INSERT INTO roles(name)
+VALUES
+('Admin'),
+('Customer');
+
+insert INTO user_roles(id_role, id_user)
+VALUES
+(1,1), 
+(2,2), 
+(2,3), 
+(1,4), 
+(1,5); 
+
+
 
 
 
